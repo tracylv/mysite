2505f3db6a8e658e4f350d3f1cad14f9cbe01b1b
Tracy Lv <lv_tengfei@163.com>
2014-04-29 15:58:34 +0800

    validate update

--- app/models/admin.js
+++ app/models/admin.js
@@ -1,19 +1,19 @@
 var Admin = function () {
 
     this.defineProperties({
-        username: {type: 'string', required: true, message: '用户名是必要项。'},
-        password: {type: 'string', required: true, message: '密码是必要项。'},
-        confirmpassword: {type: 'string', required: true, message: '密码是必要项。'},
+        username: {type: 'string', required: true, message: '用户名必须由 6 到 30 个字母、数字、下划线组成。'},
+        password: {type: 'string', required: true, message: '密码不能小于 6 个或大于 30 个字符。'},
+        confirmpassword: {type: 'string', required: true, message: '密码不能小于 6 个或大于 30 个字符。'},
         nickname: {type: 'string', required: true, message: '昵称是必要项。'},
         email: {type: 'string', required: true, message: '邮箱是必要项。'},
         issuper: {type: 'boolean', required: false},
         istop: {type: 'boolean', required: false}
     });
 
-    this.validatesLength('username', {min: 6, max: 30, message: '用户名不能小于 6 个或大于 30 个字符长度。'});
-    this.validatesLength('password', {min: 6, max: 30, message: '密码不能小于 6 个或大于 30 个字符长度。'});
+    this.validatesFormat('username', /^[a-zA-Z0-9_]{6,30}$/ ,{ message: '用户名必须由6到30个字母、数字、下划线组成。'});
+    this.validatesLength('password', {min: 6, max: 30, message: '密码不能小于 6 个或大于 30 个字符。'});
     this.validatesConfirmed('confirmpassword', 'password', { message: '两次新密码必须一致。'});
-    this.validatesLength('nickname', {min: 1, max: 30, message: '昵称不能大于20个字符长度。'});
+    this.validatesLength('nickname', {min: 1, max: 20, message: '昵称不能大于20个字符。'});
     this.validatesFormat('email', /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/, {message: '邮箱无效。'});
 
     /*
