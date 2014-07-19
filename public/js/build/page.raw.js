776cebce26db7b06e9e8f9430ee26c198a6b05ac
Tracy Lv <lv_tengfei@163.com>
2014-07-19 16:04:39 +0800

    add music player

--- public/js/build/page.raw.js
+++ public/js/build/page.raw.js
@@ -1,4 +1,4 @@
-/*! MySite 2014-07-18 */
+/*! MySite 2014-07-19 */
 /* common.js */
 
 if (typeof String.prototype.trim !== 'function') {
@@ -96,5 +96,48 @@ $(function () {
 
     //hintText
     $("input[type='text'], input[type='password']").hintText();
+
+    // audio player
+    new jPlayerPlaylist({
+        jPlayer: "#jquery_jplayer_1",
+        cssSelectorAncestor: "#jp_container_1"
+    }, [
+        {
+            title:"Trouble Is A Friend",
+            free:true,
+            mp3:"/media/audio/troubleisafriend.mp3",
+            oga:"/media/audio/troubleisafriend.ogg"
+        },
+        {
+            title:"Everything At Once",
+            free:true,
+            mp3:"/media/audio/EverythingAtOnce.mp3",
+            oga:"/media/audio/EverythingAtOnce.ogg"
+        },
+        {
+            title:"洛丽塔",
+            free:true,
+            mp3:"/media/audio/luolita.mp3",
+            oga:"/media/audio/luolita.ogg"
+        },
+        {
+            title:"皇后大道东",
+            free:true,
+            mp3:"/media/audio/huanghoudadaodong.mp3",
+            oga:"/media/audio/huanghoudadaodong.ogg"
+        }
+    ], {
+        playlistOptions: {
+            autoPlay: true
+        },
+        swfPath: "/plugins/jplayer",
+        supplied: "mp3, oga",
+        wmode: "window",
+        smoothPlayBar: true,
+        keyEnabled: true,
+        loop: true
+    });
+
+
 
 });
